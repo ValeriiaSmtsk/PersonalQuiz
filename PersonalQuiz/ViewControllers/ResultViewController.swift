6a8c96357bd404347235833666824b4b41e56a7e
Salnik Valeriia <melfed.salnik@gmail.com>
2022-03-16 01:58:32 +0300

    Remade logic in ResultViewController

--- PersonalQuiz/ViewControllers/ResultViewController.swift
+++ PersonalQuiz/ViewControllers/ResultViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 class ResultViewController: UIViewController {
     
     @IBOutlet var resultOfAnimalLabel: UILabel!
-    @IBOutlet var descriptionAnimalLabel: UILabel!
+    @IBOutlet var descriptionLabel: UILabel!
     
     var answersChosen: [Answer] = []
         
@@ -18,39 +18,50 @@ class ResultViewController: UIViewController {
         super.viewDidLoad()
         navigationItem.hidesBackButton = true
 
-        setUpLabel()
+        setUpLabels()
     }
 }
 
 // MARK: - Private Methods
 extension ResultViewController {
     
-    func setUpLabel() {
-        let kindOfAnimal = getAnimal()
+    func setUpLabels() {
+        guard let animal = getAnimal() else { return }
         
-        resultOfAnimalLabel.text = "ВЫ - \(kindOfAnimal.rawValue)!"
-        descriptionAnimalLabel.text = kindOfAnimal.definition
+        resultOfAnimalLabel.text = "Вы - \(animal.rawValue)!"
+        descriptionLabel.text = animal.definition
     }
     
-    func getAnimal() -> Animal {
-        
-        let dogCount = answersChosen.filter { $0.animal == .dog }.count
-        let catCount = answersChosen.filter { $0.animal == .cat }.count
-        let turtleCount = answersChosen.filter { $0.animal == .turtle }.count
-        let rabbitCount = answersChosen.filter { $0.animal == .rabbit }.count
-        
-        let mostRepeatAnimal = max(dogCount, catCount, rabbitCount, turtleCount)
-        
-        if mostRepeatAnimal == dogCount {
-            return .dog
-        } else if mostRepeatAnimal == catCount {
-            return .cat
-        } else if mostRepeatAnimal == rabbitCount {
-            return .rabbit
-        } else {
-            return .turtle
+    func getAnimal() -> Animal? {
+        let animalType = answersChosen.map { $0.animal }
+        let countAnimal = animalType.reduce(into: [:]) { numberOfAnimal, animal in
+            numberOfAnimal[animal, default: 0] += 1
         }
+        let mostRepeatAnimal = countAnimal.max{$0.value < $1.value }?.key
+        return mostRepeatAnimal
+        }
+        
+        
+        
+        
+        
+//        Первая попытка, выше постаралась избавиться от повторения кода
+//
+//        let dogCount = answersChosen.filter { $0.animal == .dog }.count
+//        let catCount = answersChosen.filter { $0.animal == .cat }.count
+//        let turtleCount = answersChosen.filter { $0.animal == .turtle }.count
+//        let rabbitCount = answersChosen.filter { $0.animal == .rabbit }.count
+//
+//        let mostRepeatAnimal = max(dogCount, catCount, rabbitCount, turtleCount)
+//
+//        if mostRepeatAnimal == dogCount {
+//            return .dog
+//        } else if mostRepeatAnimal == catCount {
+//            return .cat
+//        } else if mostRepeatAnimal == rabbitCount {
+//            return .rabbit
+//        } else {
+//            return .turtle
+//        }
     }
-}
-
 
